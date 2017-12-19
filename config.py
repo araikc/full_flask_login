8fae328d3fa85d165f28f1c7dd1656adc64ebc43
Arayik <araikc@gmail.com>
2017-12-19 23:57:04 +0400

    udpate

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config(object):
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     TEMPLATES_AUTO_RELOAD = True
     SESSION_TIMEOUT = 30
-    PMSECRET = "0K6541aVFfxEepLZVwOQWJwvh"
+    PMSECRET = ""
+    ADMIN_EMAIL = "araikc@gmail.com"
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -29,12 +30,12 @@ class DevelopConfig(Config):
     MAIL_USERNAME = ''
     MAIL_PASSWORD = ''
     MAIL_USERNAME = 'info@clickbuzzmedia.com'
-    MAIL_PASSWORD = 'InfoCBM99@'
-    WTF_CSRF_SECRET_KEY = '12j1j1lk31k2312313'
-    SECRET_KEY = '\x01\xc8$\x97\xd9\x1a\x13\xd9\x9eE\xabS\xc8\x17\xa4\xc3\x14\xe8Re\x94\x8cKR'
+    MAIL_PASSWORD = ''
+    WTF_CSRF_SECRET_KEY = ''
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/hpdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    SECURITY_PASSWORD_SALT = "112233445566778899"
+    SECURITY_PASSWORD_SALT = ""
     MAIL_DEFAULT_SENDER = "info@clickbuzzmedia.com"
 
